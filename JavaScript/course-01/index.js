f14e56cabb7b1ea90db934fcaede95fc6e1a91f1
LxyAdmin <tx@lxyour.com>
2017-03-26 21:11:12 +0800

    js第一节课和第二节课的知识

---
+++ JavaScript/course-01/index.js
@@ -0,0 +1,24 @@
+console.log('Hi，我的第一个Javascript应用');
+var myVar = 0.1 + 0.2;
+var bl = false;
+var bl2 = true;
+var mynum = 12 + 6 * 2;
+var mybool = mynum > 12;
+
+
+var num = 24;
+// var myresult1 = ++num % 4 + 6 * 2; // myresult 是多少呢？
+var myresult2 = num++ % 4 + 6 * 2; // myresult 是多少呢？
+
+// console.log(myresult1);
+console.log(myresult2);
+console.log(num)
+var r3 = num + 5;
+console.log(r3);
+
+
+var a = '1';
+var b = 1;
+
+console.log(a == b)
+console.log(a === b)
---
+++ JavaScript/course-02/js/index.js
@@ -0,0 +1,20 @@
+
+var arr = [1, 2, 3, 6];
+
+var arr2 = [];
+arr2[0] = 'x';
+arr2[1] = 'y';
+arr2[2] = 'z';
+
+var arr3 = [[1, 2], [3, 4]];
+
+console.log(arr.length)
+console.log(arr2.length)
+console.log(arr3.length)
+
+var myarray = new Array();
+myarray[3] = '第三个值'
+console.log(myarray);
+console.log(myarray.length);
+
+var arr5 = [];
