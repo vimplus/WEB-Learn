9052e85d3465d046a0a2b6a5f9f51b382f413c98
LxyAdmin <tx@lxyour.com>
2017-03-29 22:03:05 +0800

    add javascript-03

--- JavaScript/course-02/js/index.js
+++ JavaScript/course-02/js/index.js
@@ -1,26 +1,25 @@
-
 var arr = [1, 2, 3, 6, 8];
 
-// var arr2 = [];
-// arr2[0] = 'x';
-// arr2[1] = 'y';
-// arr2[2] = 'z';
-//
-// var arr3 = [[1, 2], [3, 4]];
-//
-// console.log(arr[0])
-// console.log(arr[1])
-// console.log(arr[2])
-// console.log(arr[3])
-// console.log(arr2.length)
-// console.log(arr3.length)
-//
-// var myarray = new Array();
-// myarray[3] = '第三个值'
-// console.log(myarray);
-// console.log(myarray.length);
-//
-// var arr5 = [];
+/*var arr2 = [];
+arr2[0] = 'x';
+arr2[1] = 'y';
+arr2[2] = 'z';
+
+var arr3 = [[1, 2], [3, 4]];
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr2.length)
+console.log(arr3.length)
+
+var myarray = new Array();
+myarray[3] = '第三个值'
+console.log(myarray);
+console.log(myarray.length);
+
+var arr5 = [];*/
 
 
 for (var i = 0; i < arr.length; i++) {
