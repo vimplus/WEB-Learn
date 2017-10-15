d11bf631498fc1e5ab5a87edecd471880a8e7e40
LxyAdmin <tx@lxyour.com>
2017-10-15 22:00:03 +0800

    update cms-demo

--- cms-demo/server/app.js
+++ cms-demo/server/app.js
@@ -51,6 +51,70 @@ router.get('/api/article/getList', co.wrap(function* (ctx, next) {
     }
 }))
 
+router.get('/api/article/delete', co.wrap(function* (ctx, next) {
+    var params = ctx.query;
+    var id = params.id;
+    var ret =yield articleModel.remove({_id: id});
+    console.log('-------ret:', ret)
+    if (ret && ret.result.n == 1) {
+        ctx.body = {
+            code: 10000,
+            data: ret.result,
+            msg: "删除成功!"
+        }
+    } else {
+        ctx.body = {
+            code: 99999,
+            data: null,
+            msg: "删除失败!"
+        }
+    }
+
+}))
+
+router.get('/api/article/findById', co.wrap(function* (ctx, next) {
+    var params = ctx.query;
+    var id = params.id;
+    var doc = yield articleModel.findOne({_id: id});
+    console.log('-------doc:', doc)
+    if (doc) {
+        ctx.body = {
+            code: 10000,
+            data: doc,
+            msg: "请求成功!"
+        }
+    } else {
+        ctx.body = {
+            code: 99999,
+            data: null,
+            msg: "获取失败!"
+        }
+    }
+
+}))
+
+router.post('/api/article/edit', co.wrap(function* (ctx, next) {
+    var params = ctx.request.body;
+    var id = params.id;
+    console.log('------id:', id)
+    var ret = yield articleModel.update({_id: id}, { $set:{title: params.title, content: params.content} });
+    console.log('-------ret:', ret)
+    if (ret && ret.n == 1) {
+        ctx.body = {
+            code: 10000,
+            data: ret,
+            msg: "修改成功!"
+        }
+    } else {
+        ctx.body = {
+            code: 99999,
+            data: null,
+            msg: "修改失败!"
+        }
+    }
+
+}))
+
 app.listen(8080, () => {
     console.log('-----恭喜你后端Node服务启动成功！运行在8080端口-----')
 })
