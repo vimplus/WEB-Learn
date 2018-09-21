978c4f957a86ba86814a11062222658960c28e0a
TangXiao <tx@vimplus.com>
2018-09-21 22:10:45 +0800

    add es6

---
+++ JavaScript-advance/A08/a.js
@@ -0,0 +1,21 @@
+function make(params) {
+    
+}
+
+function getList(params) {
+    
+}
+
+export {
+    make,
+    getList
+}
+
+var a ='乐潇游';
+var b ='es6';
+var c = 'web';
+export {
+    a as x,
+    b as y,
+    c as z
+}
---
+++ JavaScript-advance/A08/b.js
@@ -0,0 +1,12 @@
+
+
+import a from './a';
+a.make();
+
+
+import { make, x } from './a';
+make();
+
+import * as common from './a';
+common.make()
+common.getist()
